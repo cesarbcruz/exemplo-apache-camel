1d80ddab9f050da5b744bc4badea61b375993236
cesarcruz <cesar@logicsp.com.br>
2017-07-27 15:42:47 -0300

    Validator XSD

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -16,7 +17,8 @@ public class RotaPedidos {
 		//rotaPedidos.configurarSubRotas();
 		//rotaPedidos.configurarSubRotasExecutadasEmParalelo();
 		//rotaPedidos.configurarSubRotasComSeda();
-		rotaPedidos.configurarSubRotasTransformacaoXSLTComIntegracaoSOAP();
+		//rotaPedidos.configurarSubRotasTransformacaoXSLTComIntegracaoSOAP();
+		rotaPedidos.configurarRotaComvalidacaoXSD();
     }
 	
 	/**
@@ -297,6 +299,73 @@ public class RotaPedidos {
 		context.start();
 		Thread.sleep(20000);
 		context.stop();
-	}	
+	}
+	
+	/**
+	 * Transformando XML com XSLT e enviando para um WS Soap
+	 * @throws Exception
+	 */
+	private void configurarRotaComvalidacaoXSD() throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				errorHandler(
+					    deadLetterChannel("file:erro").
+					    useOriginalMessage().    
+					    logExhaustedMessageHistory(true).
+					        maximumRedeliveries(3).
+					            redeliveryDelay(5000).
+					        onRedelivery(new Processor() {            
+					            @Override
+					            public void process(Exchange exchange) throws Exception {
+					                int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+					                int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+					                System.out.println("Redelivery - " + counter + "/" + max );
+					            }
+					        })
+					);
+				
+				from("file:pedidos?delay=5s&noop=true").
+			    routeId("rota-pedidos").
+			    to("validator:pedido.xsd").
+				multicast().
+				    to("direct:soap").
+				        log("Chamando soap com ${body}").
+				    to("direct:http");
+	
+				from("direct:soap").
+				    routeId("rota-soap").
+				to("xslt:pedido-para-soap.xslt").
+				    log("Resultado do template: ${body}").
+				    setHeader(Exchange.CONTENT_TYPE,constant("text/xml")).
+				to("http4://localhost:8080/webservices/financeiro");
+	
+				from("direct:http").
+				    routeId("rota-http").
+				        setProperty("pedidoId", xpath("/pedido/id/text()")).
+				        setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+				    split().
+				        xpath("/pedido/itens/item").
+				    filter().
+				        xpath("/item/formato[text()='EBOOK']").
+				    setHeader("CamelFileName", simple("${file:name}.json")).
+				    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				    setHeader(Exchange.HTTP_QUERY,
+				                simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+				to("http4://localhost:8080/webservices/ebook/item");
+			}
+			
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}
+	
 
 }
