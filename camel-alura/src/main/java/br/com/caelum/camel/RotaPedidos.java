968f76ad9bfb6c2f7572cb521785d179d3123609
cesarcruz <cesar@logicsp.com.br>
2017-07-27 11:18:31 -0300

    Sub-rotas e mock

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -10,9 +10,10 @@ public class RotaPedidos {
 
 	public static void main(String[] args) throws Exception {
 		RotaPedidos rotaPedidos = new RotaPedidos();
-		rotaPedidos.configurarRotaArquivoXMLParaArquivoJSON();
-		rotaPedidos.configurarRotaArquivoXMLParaWebServiceHTTPPost();
-		rotaPedidos.configurarRotaArquivoXMLParaWebServiceHTTPGET();
+		//rotaPedidos.configurarRotaArquivoXMLParaArquivoJSON();
+		//rotaPedidos.configurarRotaArquivoXMLParaWebServiceHTTPPost();
+		//rotaPedidos.configurarRotaArquivoXMLParaWebServiceHTTPGET();
+		rotaPedidos.configurarSubRotas();
     }
 	
 	/**
@@ -112,5 +113,45 @@ public class RotaPedidos {
 	        Thread.sleep(2000);
 	    
 	}
+	
+	private void configurarSubRotas() throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				from("file:pedidos?delay=1s&noop=true").
+			    routeId("rota-pedidos").
+			    multicast().
+			        to("direct:soap").
+			        to("direct:http");
+
+				from("direct:soap").
+				    routeId("rota-soap").
+				    log("chamando servico soap ${body}").
+				to("mock:soap");
+	
+				from("direct:http").
+				    routeId("rota-http").
+				    setProperty("pedidoId", xpath("/pedido/id/text()")).
+				    setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+				    split().
+				        xpath("/pedido/itens/item").
+				    filter().
+				        xpath("/item/formato[text()='EBOOK']").
+				    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				    setHeader(Exchange.HTTP_QUERY,
+				            simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+				to("http4://localhost:8080/webservices/ebook/item");
+			}
+			
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}	
 
 }
